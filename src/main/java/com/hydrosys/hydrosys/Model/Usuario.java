1c3d9685cca829d246466bbf2e0e06584b15cd3a
Ricardo Perez <78655612+Richard0621@users.noreply.github.com>
2025-10-03 08:44:55 -0500

    Change fetch type of Rol relationship to EAGER

--- src/main/java/com/hydrosys/hydrosys/Model/Usuario.java
+++ src/main/java/com/hydrosys/hydrosys/Model/Usuario.java
@@ -20,7 +20,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "rol_id")
     private Rol rol;
 
